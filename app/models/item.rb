e915473e679598cf523cef0f6756eee75d0d4852
Ryan Canton <ryancanton999@gmail.com>
2023-02-07 16:47:31 -0700

    Test/Feat: Add validations to items and test them

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,7 @@ class Item < ApplicationRecord
   has_many :invoice_items, dependent: :destroy
   has_many :invoices, through: :invoice_items
   has_many :transactions, through: :invoices
+
+  validates_presence_of :name, :description, :unit_price
+  validates_numericality_of :unit_price
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Item do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should have_many(:transactions).through(:invoices) }
   end
+
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :unit_price }
+    it { should validate_presence_of :description }
+    it { should validate_numericality_of :unit_price }
+  end
 end
