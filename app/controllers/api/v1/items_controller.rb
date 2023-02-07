90d489ffd31a7555fbaee874b606237cbcd2432a
Ryan Canton <ryancanton999@gmail.com>
2023-02-07 16:44:07 -0700

    Test/Feat: Add tests and actions for item create, update, and destroy happy paths

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -8,14 +8,21 @@ class Api::V1::ItemsController < ApplicationController
   end
 
   def create
-
-  end
+    new_item = Item.create(item_params)
+    render json: ItemsSerializer.new(new_item)
+  end 
 
   def update
-
+    item = Item.update(params[:id], item_params)
+    render json: ItemsSerializer.new(item)
   end
 
   def destroy
+    render json: ItemsSerializer.new(Item.destroy(params[:id]))
+  end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
   end
 end
--- spec/requests/items_request_spec.rb
+++ spec/requests/items_request_spec.rb
@@ -75,52 +75,53 @@ describe "Items API" do
     expect(item[:attributes][:merchant_id]).to be_a(Integer)
   end
 
-  # it "can create a new book" do
-  #   book_params = ({
-  #                   title: 'Murder on the Orient Express',
-  #                   author: 'Agatha Christie',
-  #                   genre: 'mystery',
-  #                   summary: 'Filled with suspense.',
-  #                   number_sold: 432
-  #                 })
-  #   headers = {"CONTENT_TYPE" => "application/json"}
+  it "can create a new item" do
+    merchant = create(:merchant)
+    item_params = {
+      "name": "value1",
+      "description": "value2",
+      "unit_price": 100.99,
+      "merchant_id": merchant.id
+    }
+    headers = {"CONTENT_TYPE" => "application/json"}
   
-  #   # We include this header to make sure that these params are passed as JSON rather than as plain text
-  #   post "/api/v1/books", headers: headers, params: JSON.generate(book: book_params)
-  #   created_book = Book.last
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    post "/api/v1/items", headers: headers, params: JSON.generate(item: item_params)
+    created_item = Item.last
   
-  #   expect(response).to be_successful
-  #   expect(created_book.title).to eq(book_params[:title])
-  #   expect(created_book.author).to eq(book_params[:author])
-  #   expect(created_book.summary).to eq(book_params[:summary])
-  #   expect(created_book.genre).to eq(book_params[:genre])
-  #   expect(created_book.number_sold).to eq(book_params[:number_sold])
-  # end
+    expect(response).to be_successful
+    expect(created_item.name).to eq(item_params[:name])
+    expect(created_item.description).to eq(item_params[:description])
+    expect(created_item.unit_price).to eq(item_params[:unit_price])
+    expect(created_item.merchant_id).to eq(item_params[:merchant_id])
+  end
 
-  # it "can update an existing book" do
-  #   id = create(:book).id
-  #   previous_name = Book.last.title
-  #   book_params = { title: "Charlotte's Web" }
-  #   headers = {"CONTENT_TYPE" => "application/json"}
+  it "can update an existing item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant_id: merchant.id).id
+    previous_name = Item.last.name
+    item_params = { name: "Charlotte's Web" }
+    headers = {"CONTENT_TYPE" => "application/json"}
   
-  #   # We include this header to make sure that these params are passed as JSON rather than as plain text
-  #   patch "/api/v1/books/#{id}", headers: headers, params: JSON.generate({book: book_params})
-  #   book = Book.find_by(id: id)
+    # We include this header to make sure that these params are passed as JSON rather than as plain text
+    patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate({item: item_params})
+    item = Item.find_by(id: id)
   
-  #   expect(response).to be_successful
-  #   expect(book.title).to_not eq(previous_name)
-  #   expect(book.title).to eq("Charlotte's Web")
-  # end
+    expect(response).to be_successful
+    expect(item.name).to_not eq(previous_name)
+    expect(item.name).to eq("Charlotte's Web")
+  end
 
-  # it "can destroy an book" do
-  #   book = create(:book)
+  it "can destroy an item" do
+    merchant = create(:merchant)
+    id = create(:item, merchant_id: merchant.id).id
   
-  #   expect(Book.count).to eq(1)
+    expect(Item.count).to eq(1)
   
-  #   delete "/api/v1/books/#{book.id}"
+    delete "/api/v1/items/#{id}"
   
-  #   expect(response).to be_successful
-  #   expect(Book.count).to eq(0)
-  #   expect{Book.find(book.id)}.to raise_error(ActiveRecord::RecordNotFound)
-  # end
+    expect(response).to be_successful
+    expect(Item.count).to eq(0)
+    expect{Item.find(id)}.to raise_error(ActiveRecord::RecordNotFound)
+  end
 end
